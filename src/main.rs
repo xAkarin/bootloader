55da30947871e82ac035d5641ae032cb5d5aff44
lucci454 <x0akarin@proton.me>
2023-12-17 17:13:45 -0700

    TODO: impl Iterator for FloatingPointRange

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main(){
-    let test = FloatingPointRange::calc_steps(-0.3, 1.0, 0.1);
+    let test = FloatingPointRange::calc_steps_rounded(-0.3, 1.0, 0.1);
 
     print!("Test: {}\n", test); 
 
@@ -11,12 +11,26 @@ struct FloatingPointRange{
     start: f64, 
     end: f64, 
     step: f64,
+    current: f64,
+    next: f64,
 } 
 
 #[allow(dead_code)] 
 impl FloatingPointRange{
-    pub fn calc_steps(start: f64, end: f64, step: f64) -> u64 {
-        let steps: u64 = ((end - start) / step).abs().round() as u64;
-        return steps; 
+    pub fn calc_steps_rounded(start: f64, end: f64, step: f64) -> u64 {
+        return
+            ((end - start) / step).abs().round() as u64; 
     }
+    
+    pub fn calc_steps(start: f64, end: f64, step: f64) -> f64 {
+        return
+            ((end - start) / step).abs();  
+    }
+}
+
+impl Iterator for FloatingPointRange{
+    type Item = f64; 
+
+    fn next(&mut self) -> Option<Self::Item> { todo!() }
+
 }
