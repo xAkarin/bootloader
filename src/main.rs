8eb66aa647997abcb99efefa1fc20413771343e3
lucci454 <x0akarin@proton.me>
2023-12-21 19:39:57 -0700

    Starting philip op blog post about operating system in Rust as a personal project

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-fn main(){
-    todo!();
-    
-    return; 
+#![no_std] 
+#![no_main]
+
+//#[allow(dead_code)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! { 
+    loop {}
+ }
+
+use::core::panic::PanicInfo;
+
+#[panic_handler]
+#[allow(unused_variables)]
+fn panic(info: &PanicInfo) -> ! {
+     loop {} 
 }
