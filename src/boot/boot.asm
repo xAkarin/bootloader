62f18928415ca0839f9362cf60028a83e2e9b81f
Lucci454 <145884330+Lucci454@users.noreply.github.com>
2024-01-03 20:15:31 -0700

    Untested. Working through understanding OS theory and 16 bit real mode assembly.

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -5,14 +5,16 @@ org 0x7c00
 xor ax, ax
 mov cs, ax
 mov ss, ax
-mov sp, ax
+mov sp, 0x7c00
 mov ds, ax
+
+mov bp, sp
 
 push 'a'
 
-mov ax, [sp]
-
+mov ax, [ss:bp]
+ 
 int 0x10
 
 times 510 - ($ - $$) db 0
-dw 0xaa55
+dw 0xaa55   
