908a670b9771123f10c76fdb69517a75ba2b23f5
k <x0akarin@proton.me>
2024-02-14 19:16:00 -0700

    got jumping to bootstrap main done with inline assembly. TODO better comments to explain things :3

--- bootloader/bootstrap/src/main.rs
+++ bootloader/bootstrap/src/main.rs
@@ -9,13 +9,56 @@ global_asm!{r#"
     .code16
 
     __asm_entry:
+    /*
+     *  This ensures that the code segment is set to zero. 
+     *
+     *  We aren't sure if the bios jumped to
+     *  0x7c00:0x0000 or 0x0000:0x7c00 so this is important.  
+     * */
         ljmp $0, $__asm_fix_cs
 
     __asm_fix_cs: 
-        mov $0x0e, %ah
-        mov $'g', %al
-        int $0x10
-        
+    /*
+     * Here we zero all of the segment registers so that their value is known to us. 
+     * This is important as we may need to do some memory addressing with the segments and
+     * if we do not know their value we cannot accurately memory address!
+     * */
+        xor %ax, %ax
+        mov %ax, %ds
+        mov %ax, %es
+        mov %ax, %ss
+        mov %ax, %fs
+        mov %ax, %gs
+
+    /*
+     * Here we set the stack pointer to the where our bootstrap was loaded in at. 
+     * This can cause some confusion as it is a common misconception that the stack grows upwards,
+     * this is not the case. The stack grows downwards, so by setting our stack pointer here
+     * we have large enough room for a stack underneath our bootloader
+     * */
+        mov $0x7c00, %sp
+
+    /*
+     * Move the base pointer to the same memory address the stack pointer is at. TODO: unsure if
+     * this is needed
+     * */
+        mov %sp, %bp
+
+    /*
+     * Clear the direction flag so we are moving forward in memory when dealing with strings!
+     * */
+        cld
+
+    __check_13h_extensions:
+        mov $0x41, %ah
+        mov $0x55aa, %bx
+        int $0x13
+        jc spin 
+
+    __asm_rust_entry:
+        push %dx
+        call bootstrap_main
+
     spin:
         mov $0x0e, %ah
         mov $'g', %al
@@ -27,8 +70,21 @@ global_asm!{r#"
 }
 
 
+#[no_mangle]
+extern "C" fn bootstrap_main() {
+    loop {
+        bprint();
+    }  
+}
 
-
+fn bprint(){
+    unsafe{
+        asm!("mov ah, 0x0e",
+             "mov al, 'a'",
+             "int 0x10"
+            );
+    }
+}
 
 
 
