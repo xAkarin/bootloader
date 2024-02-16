4923c237161a8acf3c2d658e1e93088bbdf93d2b
Sxmourai <samourai_x@outlook.fr>
2024-02-16 15:12:51 +0100

    feature: Go to stage 1 by removing the att syntax asm

--- bootloader/stage_one/src/main.rs
+++ bootloader/stage_one/src/main.rs
@@ -7,36 +7,47 @@ use core::arch::{asm, global_asm};
 global_asm!{r#"
     /*
      * Boiler plate assembly at the start of the program to ensure it jumps to the correct function
+     * Kinda useless but enlightens my anxiety doing bootloader dev'ing
      * */
     .code16
     .global __asm_first_stage_entry
     .section .stage1_asm, "awx"
         
     __asm_first_stage_entry:
-        /*
-         * Sanity check ^^
-         * */
-        movb $0x0e, %ah
-        movb $'m', %al
-        int $0x10
-        call stage_one_main
-
-     "#, 
-    options(att_syntax)
+        jmp stage_one_main
+     "#,
 }
 
 #[no_mangle]
 extern "C" fn stage_one_main(){
-    // loop { 
-        unsafe{ 
-            asm!("mov ah, 0x0e",
-                 "mov al, \'d\'",
-                 "int 0x10"
-                );
-        }
-    // }
-
+    print("Stage 1 s");
+    chr_print(b'S');
+    chr_print(b't');
+    chr_print(b'a');
+    chr_print(b'g');
+    chr_print(b'e');
+    chr_print(b' ');
+    chr_print(b'1');
+    // loop {}
 }
+
+
+#[inline(always)]
+fn chr_print(chr: u8) {
+    unsafe {
+        asm!("mov ah, 0x0e",
+         "mov al, {}",
+         "int 0x10", in(reg_byte) chr
+        );
+    }
+}
+fn print(s: &str) {
+    for c in s.chars() {
+        chr_print(c as u8)
+    }
+}
+
+
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
